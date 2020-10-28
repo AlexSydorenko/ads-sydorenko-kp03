ee7727709dc9773fa426fa9c5aec65fab22eda2f
My Name <myEmail@example.com>
2020-10-28 18:38:17 +0200

    lab1 done

---
+++ lab1/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+using static System.Math;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter x: ");
+            double x = double.Parse(ReadLine());
+            Write("Enter y: ");
+            double y = double.Parse(ReadLine());
+            Write("Enter z: ");
+            double z = double.Parse(ReadLine());
+
+            double sqrt = Pow(x, 2) + x * Pow(y, 2) + Pow(x, 2) * z;
+            double a = Sin(x) / Sqrt(sqrt) + 2 * y;
+
+            double denom_b = Cos(a);
+            double b = Pow(x, 3) / denom_b;
+
+            if (sqrt <= 0 || denom_b == 0)
+            {
+                WriteLine("Mistake!");
+            }
+            else
+            {
+                WriteLine("a = {0}", a);
+                WriteLine("b = {0}", b);
+            }
+        }
+    }
+}
---
+++ lab1/task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+using static System.Math;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 10; num <= 9999; num++)
+            {
+                int SourseNum = num;
+                int ResNum = num;
+                int NumOfDigits = 0;
+                int sum = 0;
+                int digit;
+                do
+                {
+                    NumOfDigits++;
+                    SourseNum /= 10;
+                }
+                while (SourseNum > 0);
+
+                for (int i = 0; i < NumOfDigits; i++)
+                {
+                    digit = ResNum % 10;
+                    sum += (int)Pow(digit, NumOfDigits);
+                    ResNum /= 10;
+                }
+
+                if (sum == num)
+                {
+                    WriteLine(num);
+                }
+            }
+        }
+    }
+}
